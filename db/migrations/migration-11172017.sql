553e66851240db1bd90a33a063c3a18de06798df
Andrea McKenzie <mckenzie.andrea.m@gmail.com>
2017-11-20 14:07:28 -0500

    returned to the proper version

--- db/migrations/migration-11172017.sql
+++ db/migrations/migration-11172017.sql
@@ -12,12 +12,12 @@ CREATE TABLE IF NOT EXISTS recipes (
 
 CREATE TABLE IF NOT EXISTS ingredients (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255),
-  typical_unit VARCHAR(255)
+  name VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS ingredient_lists(
-  recipe_id INTEGER PRIMARY KEY REFERENCES recipes,
+  id SERIAL PRIMARY KEY,
+  recipe_id INTEGER REFERENCES recipes,
   ingredient_id INTEGER REFERENCES ingredients,
   amount INTEGER,
   unit VARCHAR(255)
@@ -36,9 +36,10 @@ CREATE TABLE IF NOT EXISTS users(
 --Insert ... select looks like it's going to be our best friend for most of this.
 --figure out the joins tomorrow
 
-CREATE table IF NOT EXISTS user_recipies(
-  user_id INTEGER PRIMARY KEY REFERENCES users,
-  recipe_id INTEGER REFERENCES recipies,
+CREATE table IF NOT EXISTS user_recipes(
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users,
+  recipe_id INTEGER REFERENCES recipes,
   favorited BOOLEAN,
 );
 
@@ -50,11 +51,23 @@ CREATE TABLE IF NOT EXISTS user_lists(
 
 CREATE TABLE IF NOT EXISTS shopping_lists(
   user_id INTEGER REFERENCES users,
-  id INTEGER PRIMARY KEY REFERENCES user_lists,
+  id INTEGER REFERENCES user_lists,
   ingredient_id INTEGER REFERENCES ingredients,
   amount INTEGER,
   unit VARCHAR(255)
 );
 
+--TEST QUERIES
+--create new shopping lists
+--INSERT INTO user_lists (user_id,name) values (user.id,list.name)
 
+--to add all ingredients from a recipe to a shopping list
+--INSERT INTO recipes (name,serving_size) values (recipe.name,recipe.yeild);
+--INSERT INTO ingredients_list(recipe_id,ingredient_id,amount,unit) (SELECT id from RECIPES ORDER BY id DESC LIMIT 1),[sql loop though and create # of new primary keys],[parseInt(ingredient '^\S*\s')],[ingredient('^\s*\s')]
+--INSERT INTO ingredients(name) values (recipe.ingredients[0][loop?])
+--INSERT INTO shopping_lists (user_id,ingredient_id,amount,unit) SELECT user_recipes.user_id,ingredients.id,ingredient_lists.amount, ingredient_lists.unit FROM ingredient_lists JOIN user_recipes ON ingredient_lists.recipe_id = user_recipes.recipe_id JOIN ingredients ON ingredients.id = ingredient_lists.ingredient_id WHERE ingredient_lists.recipe_id = 1;
 
+--to add an ingredient to a shopping list
+--https://stackoverflow.com/questions/20971680/sql-server-insert-if-not-exist except if exists, select the thing.
+
+--multiple queries into one transaction
