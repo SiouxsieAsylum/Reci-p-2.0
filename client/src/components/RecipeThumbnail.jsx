2ef74e511ca83942728a6aba5650942ad2670400
Osman Emara <emaraosman@gmail.com>
2017-11-21 11:59:33 -0500

    create recipethumbnail component

---
+++ client/src/components/RecipeThumbnail.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function RecipeThumbnail (props) {
+  const recipe = props.recipe;
+  
+  return(
+    <div className="recipe-thumbnail">
+      <p></p>
+    </div>
+  )
+
+}
+
+export default RecipeThumbnail;
