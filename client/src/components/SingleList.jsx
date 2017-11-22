b0c9a3357535ee098f5ee609b01d8fa2ff57d063
Steven Ciasullo <sjciasullo@gmail.com>
2017-11-22 13:58:59 -0500

    delete ozzy's console.log nonsense

--- client/src/components/SingleList.jsx
+++ client/src/components/SingleList.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 function SingleList (props) {
   const ingredient = props.ingredient;
-  console.log("ingredient DATA " + props)
   return(
     <div className="single-list">
       {ingredient.ingredientLines.map((item,index) => {
