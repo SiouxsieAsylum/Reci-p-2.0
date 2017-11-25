ff0c37ba8286673c01cb552acbb333604baee30f
Steven Ciasullo <sjciasullo@gmail.com>
2017-11-25 14:21:08 -0500

    shopping list component completion without testing

--- client/src/components/ShoppingLists.jsx
+++ client/src/components/ShoppingLists.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 function ShoppingLists(props) {
+  const shopping_lists = props.shopping_lists;
   return(
     <div className='shopping_lists'>
       <h2>User's Shopping Lists</h2>
+      <ul>
+        {shopping_lists.map( (shopping_list) => {
+          return <li key={shopping_list.list_id}>{shopping_list.name}</li>
+        })}
+      </ul>
     </div>
   )
 }
