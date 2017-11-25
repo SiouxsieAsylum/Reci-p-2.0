0bd25f385dbc774038f840596c530c6c5c072fa2
Steven Ciasullo <sjciasullo@gmail.com>
2017-11-25 12:33:09 -0500

    begin shopping lists component

---
+++ client/src/components/ShoppingLists.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ShoppingLists(props) {
+  return(
+    <div className='shopping_lists'>
+      <h2>User's Shopping Lists</h2>
+    </div>
+  )
+}
+
+export default ShoppingLists;
