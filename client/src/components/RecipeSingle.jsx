23717262a877719195f1df26bb2d98f791ea9790
Steven Ciasullo <sjciasullo@gmail.com>
2017-11-27 10:22:40 -0500

    ingredients show up for show one

--- client/src/components/RecipeSingle.jsx
+++ client/src/components/RecipeSingle.jsx
@@ -8,7 +8,13 @@ function RecipeSingle (props) {
     <div className="recipe-single">
       <h3>{recipeInfo.name}</h3><h3>Servings: {recipeInfo.serving_size}</h3>
       <div className="single-recipe-pic" style={{backgroundImage: `url(${recipeInfo.image})`}}></div>
-      <p> hi </p>
+      <ul className='recipe-ingredients'>
+        {ingredients.map(ingredient => {
+          return (<li key={ingredient.id} className='recipe-ingredient'>
+            {ingredient.amount} {ingredient.unit} {ingredient.name}
+          </li>)
+        })}
+      </ul>
     </div>
   )
 }
