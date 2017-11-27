ab5145f3087c2afe412ea4f6e5a214190e35e366
Andrea McKenzie <mckenzie.andrea.m@gmail.com>
2017-11-27 10:03:42 -0500

    all recipe endpoints should be done

--- routes/recipe-routes.js
+++ routes/recipe-routes.js
@@ -4,22 +4,15 @@ const recipeControllers = require('../controllers/recipe-controllers.js');
 
 recipeRoutes.get('/', recipeControllers.index);
 recipeRoutes.get('/:id', recipeControllers.show);
+recipeRoutes.post('/:recipe_id',recipeControllers.addIngredientsToNewRecipe)
 recipeRoutes.delete('/:recipe_id',recipeControllers.removeUserRecipe)
+recipeRoutes.delete('edit/:recipe_id/:ingredient_id',recipeControllers.removeIngredient)
 
 // new recipe and ingredients in new recipe
 recipeRoutes.post('/new',recipeControllers.createRecipe)
 recipeRoutes.post('/new/:recipe_id',recipeControllers.addIngredientsToNewRecipe)
 
 // add and remove ingredients
-recipeRoutes.post('edit/:recipe_id/:ingredient_id',recipeControllers.addIngredientsToNewRecipe)
-recipeRoutes.delete('edit/:recipe_id/:ingredient_id',recipeControllers.removeIngredient)
-
-// get the edit
-// editing ingredients will likely be its own endpoint
-// recipeRoutes.get('/:id/edit')
-
-// process the edit
-// recipeRoutes.put('/:id',recipeControllers.update)
 
 recipeRoutes.post('/:recipe_id/:shoppingList_id', recipeControllers.addRecipeToShopping);
 
