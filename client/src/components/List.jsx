cb3cfe488eac599069ff0d87655e4b888443d3e2
Steven Ciasullo <sjciasullo@gmail.com>
2017-11-27 15:14:45 -0500

    fixed key bug in mapping recipes for list

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import SingleList from './SingleList'
 
-const dummyData = require('./dummyJSON') // PLACEHOLDER DATA
-
-
 function List(props){
   const list = props.shoppingList;
   const recipes = props.shoppingRecipes;
@@ -21,9 +18,23 @@ function List(props){
     )
   }
 
+  let listRecipes = null;
+  if(recipes.length !== 0){
+    listRecipes = (
+      recipes.map((recipe, index) => {
+        return <li key={index}>{recipe.name}</li>
+      })
+    )
+  }
+
   return(
     <div className="list-container">
       <h3 className="soft-title">Ingredients list</h3>
+      
+      <ul className='shopping-recipes'>
+        {recipes.length !== 0 && <h4>Recipes</h4>}
+        {listRecipes}
+      </ul>
       {yourList}
       <form className="ingredient-form">
         <input type="text" name="addIngredient" placeholder="Add ingredient"/>
