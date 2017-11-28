29c76029428600837d865e05f32ad860d5094ccc
Steven Ciasullo <sjciasullo@gmail.com>
2017-11-28 16:49:38 -0500

    convert mainDisplay to functional Component

--- client/src/components/MainDisplay.jsx
+++ client/src/components/MainDisplay.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import RecipeForm from './RecipeForm';
 import RecipeSingle from './RecipeSingle';
@@ -7,13 +7,10 @@ import ShoppingLists from './ShoppingLists';
 import Nav from './Nav';
 
 //acts as a controller for the main display
-class MainDisplay extends Component{
-  
-  constructor(props){
-    super(props);
-    /* ------------------ Prop-list ------------------ */
+function MainDisplay(props){
+  /* ------------------ Prop-list ------------------ */
     // Variables --
-    //  auth, username, userid, apiData
+    //  auth, username, userid, apiData, apiLoaded, apiSingle, show, userShopLists
     //
     // Functions -- 
     //  loginUser(logs in user in nav),
@@ -21,78 +18,65 @@ class MainDisplay extends Component{
     //  logout(logs user out in nav)
     //  getIngredientList (get the ingredients for a shopping list, needed to open shopping lists),
     //  listFormOn (turn on the make a list form)
+    //  getUserLists (get a user's list of shopping lists)
+    //  getAllRecipes (get all of the recipes)
+    //  getSingleRecipe (get a single recipe by id)
+    //  showRecipeForm (show the edit recipe form)
+  /* ------------------    end    ------------------*/
 
-
-    this.state={
-      userShopLists: null, // holds a users shopping lists
-      apiLoaded:false,
-      // show: "recipelist", // current page for the switch
-    }
-  };
-
-
-  
-
-  render(){
-
-    let tabShow = null;
-
-    switch(this.props.show){
-      case "recipelist":
-        tabShow = (
-          <RecipeList
-            recipeToList={this.props.recipeToList}
-            recipes={this.props.apiData}
-            getSingleRecipe={this.props.getSingleRecipe}
-          />
-        )
-        break;
-      case "single":
-        tabShow = (
-          <RecipeSingle
-            userid={this.props.userid}
-            apiSingle={this.props.apiSingle}
-            getSingleRecipe={this.props.getSingleRecipe}
-          />
-        )
-        break;
-      case "form":
-        tabShow = (
-          <RecipeForm
-          userid={this.props.userid}
-          setRecipe={this.props.getSingleRecipe}
-          />
-          )
-        break;
-      case 'shoppinglist':
-        tabShow = (
-          <ShoppingLists shoppingLists={this.props.userShopLists} 
-          getIngredientsList={this.props.getIngredientsList} listFormOn={this.props.listFormOn}
-          /> 
-        )
-        break;
-      default:
-        tabShow=(
-          <p>This is the default in switch: please seek help with DRAKE</p>
-        )
-
-    }
-
-    return(
-      <div className="main-display">
-        <Nav auth={this.props.auth} loginUser={this.props.loginUser} logout={this.props.logout}
-          getAllRecipes={this.props.getAllRecipes} getUserLists={this.props.getUserLists} showRecipeForm={this.props.showRecipeForm}
+  let tabShow = null;
+  switch(this.props.show){
+    case "recipelist":
+      tabShow = (
+        <RecipeList
+          recipeToList={this.props.recipeToList}
+          recipes={this.props.apiData}
+          getSingleRecipe={this.props.getSingleRecipe}
         />
+      )
+      break;
+    case "single":
+      tabShow = (
+        <RecipeSingle
+          userid={this.props.userid}
+          apiSingle={this.props.apiSingle}
+          getSingleRecipe={this.props.getSingleRecipe}
+        />
+      )
+      break;
+    case "form":
+      tabShow = (
+        <RecipeForm
+        userid={this.props.userid}
+        setRecipe={this.props.getSingleRecipe}
+        />
+        )
+      break;
+    case 'shoppinglist':
+      tabShow = (
+        <ShoppingLists shoppingLists={this.props.userShopLists} 
+        getIngredientsList={this.props.getIngredientsList} listFormOn={this.props.listFormOn}
+        /> 
+      )
+      break;
+    default:
+      tabShow=(
+        <p>This is the default in switch: please seek help with DRAKE</p>
+      )
+  }
+  
+  return(
+    <div className="main-display">
+      <Nav auth={this.props.auth} loginUser={this.props.loginUser} logout={this.props.logout}
+        getAllRecipes={this.props.getAllRecipes} getUserLists={this.props.getUserLists} showRecipeForm={this.props.showRecipeForm}
+      />
 
-        {this.props.apiLoaded  && (
-          tabShow
-        )}
+      {this.props.apiLoaded  && (
+        tabShow
+      )}
 
-      </div>
-    )
-  }//end of render
-
-
+    </div>
+  )
 };//End of  Component MainDisplay
 
 
